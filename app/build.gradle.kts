fb4fed990317dd3cd077b3ea2eab216913fdeba7
feher.daniel <feherdani8@gmail.com>
2025-02-14 00:44:53 +0100

    Add drinkservice, retrofit client and drink classes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,8 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.converter.gson)
+    implementation(libs.retrofit)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
